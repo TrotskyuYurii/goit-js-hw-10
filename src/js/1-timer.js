09d5aed9cb57ee2e0f974f3a72e1e7167a09cdc2
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-01-15 21:14:00 +0200

    Update package.json and add flatpickr timer functionality

---
+++ src/js/1-timer.js
@@ -0,0 +1,15 @@
+const options = {
+        enableTime: true,
+        time_24hr: true,
+        defaultDate: new Date(),
+        minuteIncrement: 1,
+        onClose(selectedDates) {
+            console.log(selectedDates[0]);
+            
+        },
+    };
+
+    document.addEventListener('DOMContentLoaded', function() {
+        flatpickr('#datetime-picker', options);
+    });
+    
