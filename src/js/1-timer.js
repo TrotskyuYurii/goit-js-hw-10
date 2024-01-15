25fd55735fa57a534e0355ca36864a81e1bfbef3
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-01-15 21:52:11 +0200

    Add defer attribute to script tag in 1-timer.html and validate selected date in 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,15 +1,30 @@
-const options = {
-        enableTime: true,
-        time_24hr: true,
-        defaultDate: new Date(),
-        minuteIncrement: 1,
-        onClose(selectedDates) {
-            console.log(selectedDates[0]);
-            
-        },
-    };
+let userSelectedDate = undefined;
+const startButton = document.querySelector('[data-start]');
 
-    document.addEventListener('DOMContentLoaded', function() {
-        flatpickr('#datetime-picker', options);
-    });
-    
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      window.alert('Please choose a date in the future');
+      return;
+    } else {
+      startButton.disabled = false;
+      userSelectedDate = selectedDates[0];
+    }
+  },
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  //запуск flatpickr
+  flatpickr('#datetime-picker', options);
+
+  // Обробка кнопки Start
+  startButton.disabled = true;
+
+  startButton.addEventListener('click', function () {
+    console.log(userSelectedDate);
+  });
+});
