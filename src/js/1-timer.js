3af78880a99d8f01d89030e711afc3fa1671ee77
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-01-15 22:10:45 +0200

    Add iziToast library and initialize flatpickr

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,7 @@
 let userSelectedDate = undefined;
 const startButton = document.querySelector('[data-start]');
 
+//ініціалізація flatpickr
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -8,7 +9,12 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] <= new Date()) {
-      window.alert('Please choose a date in the future');
+      startButton.disabled = true;
+    //   window.alert('Please choose a date in the future');
+    iziToast.show({
+        title: 'Hey',
+        message: 'What would you like to add?'
+    });
       return;
     } else {
       startButton.disabled = false;
@@ -16,6 +22,27 @@ const options = {
     }
   },
 };
+
+//Функция для подсчета времени
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+
 
 document.addEventListener('DOMContentLoaded', function () {
   //запуск flatpickr
